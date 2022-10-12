5ef0856b5e10997736e58a575c73c7ff9b75c30f
jackiieefa <jackiieefa@gmail.com>
2022-10-12 11:46:44 -0600

    Some Color + Customiztion

--- www/js/index.js
+++ www/js/index.js
@@ -2,5 +2,19 @@ var app = new Framework7({
     // App root element
     el: '#app',
     // ... other parameters
+    routes: [
+      {
+        path: '/',
+        url: 'index.html',
+      },
+      { // do this for all pages!
+        path: '/page2/',
+        url: 'pages/page2.html',
+      },
+      { // do this for all pages!
+        path: '/page3/',
+        url: 'pages/page3.html',
+      },
+    ],
   });
 var mainView = app.views.create('.view-main')
